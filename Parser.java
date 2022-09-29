8694ecd0a60145d6469f07df563939db46e7fa5e
Sasha Yeutseyeva <syeutseyeva@smith.edu>
2022-09-29 15:30:26 -0400

    add comments

--- Parser.java
+++ Parser.java
@@ -10,7 +10,7 @@ public class Parser {
    
    public static void main(String[] args) throws Exception{
 
-        File inputFile = new File("smallTest.xml");
+        File inputFile = new File("test.xml");
 
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -31,8 +31,14 @@ public class Parser {
         
             if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                 Element eElement = (Element) nNode;
+
+                // get ID of the requirement in the document, this is an attribute of the element (<req id="1">)
+                // the id isn't necessarily useful if the IDs are repeated for different sections
                 System.out.println("Requirement ID: " 
                     + eElement.getAttribute("id"));
+                    
+                // gets the text of the requirement, we assume it's in a <text_body> element 
+                // TODO(sashayeu): make sure the requirement stays on one line, currently if there are two <text_body> elements the requirement shows up in 2 lines 
                 System.out.println("Requirement Description: " 
                     + eElement
                     .getElementsByTagName("text_body")
