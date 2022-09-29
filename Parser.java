3b0217cfb48974570aa55e88b1a9b35ad1aee429
Sasha Yeutseyeva <syeutseyeva@smith.edu>
2022-09-29 15:58:49 -0400

    parser outputs to csv

--- Parser.java
+++ Parser.java
@@ -5,47 +5,54 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Parser {
    
    public static void main(String[] args) throws Exception{
 
-        File inputFile = new File("test.xml");
-
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-
-        Document doc = dBuilder.parse(inputFile);
-        doc.getDocumentElement().normalize();
-
-        System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-
-        // This immediately gets all the requirements that have a <req> element annotation
-        NodeList nList = doc.getElementsByTagName("req");
+        NodeList nList = extractRequirements("smallTest.xml");
         // now that the requirements are stored in nList, all we need to do is print them out cohesively 
-        System.out.println("----------------------------");
-        
-        for (int temp = 0; temp < nList.getLength(); temp++) {
-            Node nNode = nList.item(temp);
-            System.out.println("\nCurrent Element :" + nNode.getNodeName());
-        
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element eElement = (Element) nNode;
+        createCSV(nList);
+   }
 
-                // get ID of the requirement in the document, this is an attribute of the element (<req id="1">)
-                // the id isn't necessarily useful if the IDs are repeated for different sections
-                System.out.println("Requirement ID: " 
-                    + eElement.getAttribute("id"));
-                    
-                // gets the text of the requirement, we assume it's in a <text_body> element 
-                // TODO(sashayeu): make sure the requirement stays on one line, currently if there are two <text_body> elements the requirement shows up in 2 lines 
-                System.out.println("Requirement Description: " 
-                    + eElement
-                    .getElementsByTagName("text_body")
-                    .item(0)
-                    .getTextContent());
-            }
+   public static NodeList extractRequirements(String filename) throws Exception{
+
+    File inputFile = new File(filename);
+
+    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+
+    Document doc = dBuilder.parse(inputFile);
+    doc.getDocumentElement().normalize();
+
+    // This immediately gets all the requirements that have a <req> element annotation
+    NodeList nList = doc.getElementsByTagName("req");
+
+    return nList;
+   }
+
+   public static void createCSV(NodeList nList) throws Exception{
+
+    File outputCSV = new File("output.csv");
+    PrintWriter pw = new PrintWriter(new FileWriter(outputCSV));
+
+    for (int temp = 0; temp < nList.getLength(); temp++) {
+        Node nNode = nList.item(temp);
+    
+        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+            Element eElement = (Element) nNode;
+
+            String id = eElement.getAttribute("id").toString();
+            String requirement = eElement.getElementsByTagName("text_body").item(0).getTextContent().toString();
+            requirement = requirement.replaceAll("\n", "");
+
+            pw.print(String.format("%s, %s%n", id, requirement));
         }
+    }
 
+    pw.close();
    }
 }
