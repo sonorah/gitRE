223d290fd58777e28c393668e93434ae91335bd7
Sasha Yeutseyeva <syeutseyeva@smith.edu>
2022-09-29 15:25:02 -0400

    renamed sonora's work to DomParserDemo and added Parser file as well as a small test file

---
+++ DomParserDemo.java
@@ -0,0 +1,44 @@
+package com.tutorialspoint.xml;
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+public class DomParserDemo {
+
+   
+   public static void main(String[] args){
+        try {
+            File inputFile = new File("test2.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+            NodeList nList = doc.getElementsByTagName("req");
+            System.out.println("----------------------------");
+            
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                System.out.println("\nCurrent Element :" + nNode.getNodeName());
+            
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    System.out.println("Requirement ID: " 
+                        + eElement.getAttribute("id"));
+                    System.out.println("Requirement Description: " 
+                        + eElement
+                        .getElementsByTagName("text_body")
+                        .item(0)
+                        .getTextContent());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+   }
+}
--- Parser.java
+++ Parser.java
@@ -1,4 +1,3 @@
-package com.tutorialspoint.xml;
 import java.io.File;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
@@ -7,37 +6,39 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 
-public class DomParserDemo {
-
+public class Parser {
    
-   public static void main(String[] args) {
-        try {
-            File inputFile = new File("test2.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputFile);
-            doc.getDocumentElement().normalize();
-            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-            NodeList nList = doc.getElementsByTagName("req");
-            System.out.println("----------------------------");
-            
-            for (int temp = 0; temp < nList.getLength(); temp++) {
-                Node nNode = nList.item(temp);
-                System.out.println("\nCurrent Element :" + nNode.getNodeName());
-            
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
-                    System.out.println("Requirement ID: " 
-                        + eElement.getAttribute("id"));
-                    System.out.println("Requirement Description: " 
-                        + eElement
-                        .getElementsByTagName("text_body")
-                        .item(0)
-                        .getTextContent());
-                }
+   public static void main(String[] args) throws Exception{
+
+        File inputFile = new File("smallTest.xml");
+
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+
+        Document doc = dBuilder.parse(inputFile);
+        doc.getDocumentElement().normalize();
+
+        System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+        // This immediately gets all the requirements that have a <req> element annotation
+        NodeList nList = doc.getElementsByTagName("req");
+        // now that the requirements are stored in nList, all we need to do is print them out cohesively 
+        System.out.println("----------------------------");
+        
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+            Node nNode = nList.item(temp);
+            System.out.println("\nCurrent Element :" + nNode.getNodeName());
+        
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) nNode;
+                System.out.println("Requirement ID: " 
+                    + eElement.getAttribute("id"));
+                System.out.println("Requirement Description: " 
+                    + eElement
+                    .getElementsByTagName("text_body")
+                    .item(0)
+                    .getTextContent());
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
 
    }
